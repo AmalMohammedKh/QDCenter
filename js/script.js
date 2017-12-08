df0ef728b89cb517850fe97e760af5e82bcf1449
AmalMohammedKh <34216360+AmalMohammedKh@users.noreply.github.com>
2017-12-08 18:17:27 +0200

    Main page Design and Coding for Quality and Development Ceter in Islamic University

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.p-menu-icon').click(function () {
+        $(".p-menu-row").slideToggle("slow", function() {
+            // Animation complete.
+        });
+    });
+
+
+});
+
